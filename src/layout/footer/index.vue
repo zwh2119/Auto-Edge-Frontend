4ccc4247e9e27e73d1d5872869899ebc0e6b911e
AdaYangOlzz <adayangolzz@163.com>
2023-09-06 16:00:44 +0800

    simplified

---
+++ src/layout/footer/index.vue
@@ -0,0 +1,18 @@
+
+
+<script setup lang="ts" name="layoutFooter">
+// 此处需有内容（注释也得），否则缓存将失败
+</script>
+
+<style scoped lang="scss">
+.layout-footer {
+	width: 100%;
+	display: flex;
+	&-warp {
+		margin: auto;
+		color: var(--el-text-color-secondary);
+		text-align: center;
+		animation: error-num 0.3s ease;
+	}
+}
+</style>
---
+++ src/views/result/index.vue
@@ -0,0 +1,3 @@
+<template>
+    result
+</template>
